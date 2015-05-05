34a8a2603d348a617c6ad142a44a4ba86955a4c8
David Newbury <david.newbury@gmail.com>
2015-05-05 18:06:23 -0400

    relative links

--- config.rb
+++ config.rb
@@ -38,7 +38,8 @@ activate :bh
 
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
-
+set :relative_links, true
+activate :relative_assets
 
 activate :deploy do |deploy|
   deploy.method = :git
